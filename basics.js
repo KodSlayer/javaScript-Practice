575711708cc6ccee0e3c20482337b1fea3b06946
KodSlayer <yashasteshi08@gmail.com>
2025-03-07 18:04:27 +0530

    Learned JavaScript Rest API

---
+++ basics.js
@@ -0,0 +1,57 @@
+// 5 type 
+//console.log("string")
+//alert("hi")
+//cofirm('hi)
+//prompt('hey') -> this accepts string inputs 
+//document.write('hi') to print someting in the UI
+//--------------------------------------------------------------------------
+// let num1 = parseInt(prompt("please enter number1"))
+// let num2 = parseInt(prompt("please enter number2"))
+// function yash(a,b) {
+
+//     console.log(a+b)
+// }
+
+// yash(num1,num2)
+
+//-----------------------------------------------------------------------------
+// Array1   
+
+// let a = [10,20,30]
+// console.log(a)
+
+// let b = Array.of(10,20,30)
+// console.log(b)
+
+// let c=new Array(10,20,30) 
+// console.log(c)
+
+// a[1] = 'hi'
+// console.log(a)
+
+// delete a[1]
+
+// console.log(a)
+
+// let a = fetch('https://dummyjson.com/products')
+// a.then((m)=>{
+//     let data = m.json()
+//     data.then((n)=>{
+//         console.log(n)
+//         n.products.map((j)=>{
+//             console.log(j)
+//             try {
+//             let i = document.getElementById('data')
+//             i.innerHTML += `<h1>${j.title}</h1>
+//             <img src="${j.images}" height='100px' width='100px'>`
+//             } catch(error) {
+//             let i = document.getElementById('data')
+//             i.innerHTML += `<h1>${j.title}</h1>
+//             <img src="${j.images}" height='100px' width='100px'>`
+//             }
+//         })
+//     })
+// })
+
+
+
